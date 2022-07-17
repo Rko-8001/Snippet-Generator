bcbaffe5b2d8d01ccaee22f9c1eec0102afa31e2
Rko_8001 <yadwindersingh0705@gmail.com>
2022-07-17 10:09:22 +0530

    config all files

---
+++ src/components/input/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../style/style.css";
+
+const Input = (props) => {
+    return(
+        <>
+        </>
+    );
+};
+
+export default Input;
---
+++ src/components/output/Output.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../style/style.css";
+
+const Ouput = (props) => {
+
+    return (
+        <>
+        </>
+    );
+    
+};
+
+export default Output;
