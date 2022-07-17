7b5a714ff0a00537df9d60f3a8c38045b183c441
Rko_8001 <yadwindersingh0705@gmail.com>
2022-07-17 11:30:36 +0530

    Input passes backed to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './header/Header';
+import Input from './input/Input';
+import Output from './output/Output';
+import "./style/App.css";
 function App() {
+
+  const [input, inputUpdate] = useState({
+    des: '',
+    tab: '',
+    html: '',
+  })
+  function saveInputCode(inputCode) {
+    inputUpdate(inputCode);
+    // console.log(input);
+  }
   return (
     <>
+      <div className="main">
+        <Header />
+        <div className="sub-main">
+          <Input onSaveInputCode={saveInputCode} />
+        </div>
+        <Output des={input.des} tab={input.tab} html={input.html} />
+      </div>
     </>
   );
 }
